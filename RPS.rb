6fe35b4ef72adcbaad6fc4ac531254e0214acc5b
marielastancheva <mariela_stan4eva@abv.bg>
2014-06-13 21:44:26 +0300

    until<->while

--- RPS.rb
+++ RPS.rb
@@ -1,12 +1,12 @@
 # result_table = Array.new(3) { Array.new(3) }
 result_table = [["Remi", "Win", "Lose"], ["Lose", "Remi", "Win"], ["Win", "Lose", "Remi"]]
-choises=["R","P","S"]
+choises = ["R","P","S"]
 begin 
 	puts "Player choise: please input one of (R)ock; (P)aper; (S)cissorc"
-	player_choise=gets
-	player_choise=choises.index(player_choise.strip)
-end until not(player_choise==nil)
+	player_choise = gets
+	player_choise = choises.index(player_choise.strip)
+end while player_choise == nil
 
-computer_choise=rand(3)
-puts "Computer choise "+choises[computer_choise]
-puts "Player "+result_table[computer_choise][player_choise]
+computer_choise = rand(3)
+puts "Computer choise " + choises[computer_choise]
+puts "Player " + result_table[computer_choise][player_choise]
