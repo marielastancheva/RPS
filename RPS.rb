8ba3f4e3e1c98c2d390c0c2d66e50a879f1f99b2
marielastancheva <mariela_stan4eva@abv.bg>
2014-06-13 21:20:17 +0300

    Character input

--- RPS.rb
+++ RPS.rb
@@ -1,8 +1,9 @@
 # result_table = Array.new(3) { Array.new(3) }
 result_table = [["Remi", "Win", "Lose"], ["Lose", "Remi", "Win"], ["Win", "Lose", "Remi"]]
-puts "Your choise 0=Rock;1=Paper;2=Scissorc"
-nr1=gets
-nr3=nr1.to_i
-nr2=rand(3)
-puts "Computer choise "+nr2.to_s
-puts result_table[nr3][nr2]
+choises=["R","P","S"]
+puts "Player choise: (R)ock; (P)aper; (S)cissorc"
+player_choise=gets
+player_choise=choises.index(player_choise.strip)
+computer_choise=rand(3)
+puts "Computer choise "+choises[computer_choise]
+puts "Player "+result_table[player_choise][computer_choise]
