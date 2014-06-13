7ab450bb784db7f8050b15e6ccb2ae931d9b9723
marielastancheva <mariela_stan4eva@abv.bg>
2014-06-13 21:40:22 +0300

    Input validation

--- RPS.rb
+++ RPS.rb
@@ -1,9 +1,12 @@
 # result_table = Array.new(3) { Array.new(3) }
 result_table = [["Remi", "Win", "Lose"], ["Lose", "Remi", "Win"], ["Win", "Lose", "Remi"]]
 choises=["R","P","S"]
-puts "Player choise: (R)ock; (P)aper; (S)cissorc"
-player_choise=gets
-player_choise=choises.index(player_choise.strip)
+begin 
+	puts "Player choise: please input one of (R)ock; (P)aper; (S)cissorc"
+	player_choise=gets
+	player_choise=choises.index(player_choise.strip)
+end until not(player_choise==nil)
+
 computer_choise=rand(3)
 puts "Computer choise "+choises[computer_choise]
-puts "Player "+result_table[player_choise][computer_choise]
+puts "Player "+result_table[computer_choise][player_choise]
