4bb89e5f5d308a1943839b3247ba948fd0cc3066
marielastancheva <mariela_stan4eva@abv.bg>
2014-06-27 21:50:23 +0300

    Points added

--- RPS.rb
+++ RPS.rb
@@ -1,6 +1,9 @@
 # result_table = Array.new(3) { Array.new(3) }
 result_table = [["Remi", "Win", "Lose"], ["Lose", "Remi", "Win"], ["Win", "Lose", "Remi"]]
 choises = ["R","P","S"]
+player_result=0
+computer_result=0
+begin
 begin 
 	puts "Player choise: please input one of (R)ock; (P)aper; (S)cissorc"
 	player_choise = gets
@@ -10,10 +13,12 @@ end while player_choise == nil
 computer_choise = rand(3)
 puts "Computer choise " + choises[computer_choise]
 puts "Player " + result_table[computer_choise][player_choise]
-
-
-# {
-# 	"R" => "S",
-# 	"S" => "P",
-# 	"P" => "R",
-# }
+if result_table[computer_choise][player_choise]=="Win" 
+	player_result=player_result+1
+elsif result_table[computer_choise][player_choise]=="Lose"
+	computer_result=computer_result+1
+end
+puts "Player: "+player_result.to_s+" Computer: "+computer_result.to_s
+puts "Would you like to play again? Y/N"
+player_answer = gets
+end while player_answer.strip == "Y"
